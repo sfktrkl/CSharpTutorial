c1f901e45deaabc4b9cd938264675d57396656fc
Safak TURKELI <sfktrkl@gmail.com>
2022-01-30 18:32:27 +0300

    Functions Used in Console Environment to Write and Read

--- ConsoleApplication/Program.cs
+++ ConsoleApplication/Program.cs
@@ -6,7 +6,20 @@ namespace ConsoleApplication
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Functions Used in Console Environment to Write and Read
+            Lesson10();
         }
+
+        // Functions Used in Console Environment to Write and Read
+        static void Lesson10()
+        {
+            // Cursor stop at the end of the text.
+            Console.Write("Hello");
+            // Cursor stop at the bottom line of the text.
+            Console.WriteLine("Hello");
+            // To see the cursor.
+            Console.ReadKey();
+        }
+
     }
 }
