35558c0e681a306a9eb10fde0c340f56444a2889
Safak TURKELI <sfktrkl@gmail.com>
2022-01-30 18:54:17 +0300

    Type Conversions in Variables

--- ConsoleApplication/Program.cs
+++ ConsoleApplication/Program.cs
@@ -12,6 +12,8 @@ namespace ConsoleApplication
             //Lesson11();
             // What Is Variables in C# ?
             //Lesson12()
+            // Type Conversions in Variables
+            Lesson13();
         }
 
         // Functions Used in Console Environment to Write and Read
@@ -64,6 +66,38 @@ namespace ConsoleApplication
             // Case sensitive
             string sensitive;
             string Sensitive;
+        }
+
+        // Type Conversions in Variables
+        // Implicit casting
+        // When assign a value of a smaller data type to a bigger one.
+        // byte, short, int, long, float, double
+        // Explicit casting
+        // Where automatic conversion cannot be done or
+        // while assign a value of a bigger data type to a smaller one or
+        // to get the desired type to convert the specified value.
+        static void Lesson13()
+        {
+            // Explicit casting
+
+            // Cast operator
+            double x = 24;
+            int y;
+            y = (int)x;
+            Console.WriteLine(y);
+
+            // Convert class
+            double k = 24;
+            int l;
+            l = Convert.ToInt32(k);
+            Console.WriteLine(l);
+
+            double m = 24;
+            int n = 12;
+            float o = 6;
+            char c = '+';
+            string s = x.ToString() + n.ToString() + o.ToString() + c.ToString();
+            Console.WriteLine(s);
         }
 
     }
