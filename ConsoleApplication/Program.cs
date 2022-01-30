efa94d1cc65d7cfa9099c4e751bf4fb4160c18e1
Safak TURKELI <sfktrkl@gmail.com>
2022-01-30 18:37:33 +0300

    Read & Readline

--- ConsoleApplication/Program.cs
+++ ConsoleApplication/Program.cs
@@ -7,7 +7,9 @@ namespace ConsoleApplication
         static void Main(string[] args)
         {
             // Functions Used in Console Environment to Write and Read
-            Lesson10();
+            //Lesson10();
+            // Read & ReadLine
+            Lesson11();
         }
 
         // Functions Used in Console Environment to Write and Read
@@ -19,6 +21,20 @@ namespace ConsoleApplication
             Console.WriteLine("Hello");
             // To see the cursor.
             Console.ReadKey();
+        }
+
+        // Read & ReadLine
+        static void Lesson11()
+        {
+            Console.WriteLine("What is your name?");
+            // Used to retrieve the data (string)
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello " + name);
+
+            Console.WriteLine("What is your name again?");
+            // Used to retrieve the data as first char (int, ascii code)
+            int ascii = Console.Read();
+            Console.WriteLine("First character ASCII " + ascii);
         }
 
     }
