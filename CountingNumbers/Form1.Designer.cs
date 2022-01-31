2f9229035d8e8ea1b294df4042807c7b285ec776
Safak TURKELI <sfktrkl@gmail.com>
2022-02-01 02:52:05 +0300

    Counting numbers project

---
+++ CountingNumbers/Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿
+namespace CountingNumbers
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxStart = new System.Windows.Forms.TextBox();
+            this.textBoxEnd = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBoxDivisible = new System.Windows.Forms.ComboBox();
+            this.textBoxNumbers = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButtonGreen = new System.Windows.Forms.RadioButton();
+            this.radioButtonBlue = new System.Windows.Forms.RadioButton();
+            this.radioButtonRed = new System.Windows.Forms.RadioButton();
+            this.radioButtonBlack = new System.Windows.Forms.RadioButton();
+            this.checkBoxBold = new System.Windows.Forms.CheckBox();
+            this.checkBoxItalic = new System.Windows.Forms.CheckBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBoxStart
+            // 
+            this.textBoxStart.Location = new System.Drawing.Point(73, 6);
+            this.textBoxStart.Name = "textBoxStart";
+            this.textBoxStart.Size = new System.Drawing.Size(100, 20);
+            this.textBoxStart.TabIndex = 0;
+            this.textBoxStart.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox_KeyPress);
+            // 
+            // textBoxEnd
+            // 
+            this.textBoxEnd.Location = new System.Drawing.Point(201, 6);
+            this.textBoxEnd.Name = "textBoxEnd";
+            this.textBoxEnd.Size = new System.Drawing.Size(100, 20);
+            this.textBoxEnd.TabIndex = 1;
+            this.textBoxEnd.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Start From";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(179, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(16, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "to";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 122);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Count";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(96, 122);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Exit";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 39);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Divisible";
+            // 
+            // comboBoxDivisible
+            // 
+            this.comboBoxDivisible.FormattingEnabled = true;
+            this.comboBoxDivisible.Location = new System.Drawing.Point(73, 36);
+            this.comboBoxDivisible.Name = "comboBoxDivisible";
+            this.comboBoxDivisible.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxDivisible.TabIndex = 6;
+            // 
+            // textBoxNumbers
+            // 
+            this.textBoxNumbers.Location = new System.Drawing.Point(15, 63);
+            this.textBoxNumbers.Multiline = true;
+            this.textBoxNumbers.Name = "textBoxNumbers";
+            this.textBoxNumbers.Size = new System.Drawing.Size(286, 53);
+            this.textBoxNumbers.TabIndex = 7;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButtonGreen);
+            this.groupBox1.Controls.Add(this.radioButtonBlue);
+            this.groupBox1.Controls.Add(this.radioButtonRed);
+            this.groupBox1.Controls.Add(this.radioButtonBlack);
+            this.groupBox1.Location = new System.Drawing.Point(308, 6);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(102, 110);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Select Color";
+            // 
+            // radioButtonGreen
+            // 
+            this.radioButtonGreen.AutoSize = true;
+            this.radioButtonGreen.Location = new System.Drawing.Point(6, 87);
+            this.radioButtonGreen.Name = "radioButtonGreen";
+            this.radioButtonGreen.Size = new System.Drawing.Size(54, 17);
+            this.radioButtonGreen.TabIndex = 0;
+            this.radioButtonGreen.Text = "Green";
+            this.radioButtonGreen.UseVisualStyleBackColor = true;
+            this.radioButtonGreen.CheckedChanged += new System.EventHandler(this.radioButton_CheckedChanged);
+            // 
+            // radioButtonBlue
+            // 
+            this.radioButtonBlue.AutoSize = true;
+            this.radioButtonBlue.Location = new System.Drawing.Point(6, 66);
+            this.radioButtonBlue.Name = "radioButtonBlue";
+            this.radioButtonBlue.Size = new System.Drawing.Size(46, 17);
+            this.radioButtonBlue.TabIndex = 0;
+            this.radioButtonBlue.Text = "Blue";
+            this.radioButtonBlue.UseVisualStyleBackColor = true;
+            this.radioButtonBlue.CheckedChanged += new System.EventHandler(this.radioButton_CheckedChanged);
+            // 
+            // radioButtonRed
+            // 
+            this.radioButtonRed.AutoSize = true;
+            this.radioButtonRed.Location = new System.Drawing.Point(6, 43);
+            this.radioButtonRed.Name = "radioButtonRed";
+            this.radioButtonRed.Size = new System.Drawing.Size(45, 17);
+            this.radioButtonRed.TabIndex = 0;
+            this.radioButtonRed.Text = "Red";
+            this.radioButtonRed.UseVisualStyleBackColor = true;
+            this.radioButtonRed.CheckedChanged += new System.EventHandler(this.radioButton_CheckedChanged);
+            // 
+            // radioButtonBlack
+            // 
+            this.radioButtonBlack.AutoSize = true;
+            this.radioButtonBlack.Checked = true;
+            this.radioButtonBlack.Location = new System.Drawing.Point(7, 20);
+            this.radioButtonBlack.Name = "radioButtonBlack";
+            this.radioButtonBlack.Size = new System.Drawing.Size(52, 17);
+            this.radioButtonBlack.TabIndex = 0;
+            this.radioButtonBlack.TabStop = true;
+            this.radioButtonBlack.Text = "Black";
+            this.radioButtonBlack.UseVisualStyleBackColor = true;
+            this.radioButtonBlack.CheckedChanged += new System.EventHandler(this.radioButton_CheckedChanged);
+            // 
+            // checkBoxBold
+            // 
+            this.checkBoxBold.AutoSize = true;
+            this.checkBoxBold.Location = new System.Drawing.Point(308, 122);
+            this.checkBoxBold.Name = "checkBoxBold";
+            this.checkBoxBold.Size = new System.Drawing.Size(47, 17);
+            this.checkBoxBold.TabIndex = 9;
+            this.checkBoxBold.Text = "Bold";
+            this.checkBoxBold.UseVisualStyleBackColor = true;
+            this.checkBoxBold.CheckedChanged += new System.EventHandler(this.checkBox_CheckedChanged);
+            // 
+            // checkBoxItalic
+            // 
+            this.checkBoxItalic.AutoSize = true;
+            this.checkBoxItalic.Location = new System.Drawing.Point(361, 122);
+            this.checkBoxItalic.Name = "checkBoxItalic";
+            this.checkBoxItalic.Size = new System.Drawing.Size(48, 17);
+            this.checkBoxItalic.TabIndex = 9;
+            this.checkBoxItalic.Text = "Italic";
+            this.checkBoxItalic.UseVisualStyleBackColor = true;
+            this.checkBoxItalic.CheckedChanged += new System.EventHandler(this.checkBox_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(420, 154);
+            this.Controls.Add(this.checkBoxItalic);
+            this.Controls.Add(this.checkBoxBold);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.textBoxNumbers);
+            this.Controls.Add(this.comboBoxDivisible);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxEnd);
+            this.Controls.Add(this.textBoxStart);
+            this.Name = "Form1";
+            this.Text = "Counting numbers";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxStart;
+        private System.Windows.Forms.TextBox textBoxEnd;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBoxDivisible;
+        private System.Windows.Forms.TextBox textBoxNumbers;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButtonGreen;
+        private System.Windows.Forms.RadioButton radioButtonBlue;
+        private System.Windows.Forms.RadioButton radioButtonRed;
+        private System.Windows.Forms.RadioButton radioButtonBlack;
+        private System.Windows.Forms.CheckBox checkBoxBold;
+        private System.Windows.Forms.CheckBox checkBoxItalic;
+    }
+}
+
---
+++ CountingNumbers/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System.Windows.Forms;
+using System.Drawing;
+using System;
+
+namespace CountingNumbers
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] numbers = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+            this.comboBoxDivisible.Items.AddRange(numbers);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (this.textBoxStart.Text.Trim() == "" || this.textBoxEnd.Text.Trim() == "")
+                MessageBox.Show("Fill the boxes");
+            else if (this.comboBoxDivisible.SelectedIndex == -1)
+                MessageBox.Show("Select divisible");
+            else
+            {
+                textBoxNumbers.Clear();
+                var controlCount = 1;
+                var firstNumber = Convert.ToInt32(this.textBoxStart.Text);
+                var secondNumber = Convert.ToInt32(this.textBoxEnd.Text);
+                var divisibleNumber = Convert.ToInt32(this.comboBoxDivisible.SelectedItem);
+
+                for (int i = firstNumber; i < secondNumber; i++)
+                {
+                    if (i % divisibleNumber == 0)
+                    {
+                        if (controlCount % 5 == 0)
+                            textBoxNumbers.Text += Environment.NewLine;
+                        textBoxNumbers.Text += i.ToString() + " ";
+                        controlCount++;
+                    }
+                }
+            }
+        }
+
+        private void textBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar))
+                e.Handled = true;
+        }
+
+        private void radioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            if (this.radioButtonBlack.Checked)
+                this.textBoxNumbers.ForeColor = Color.Black;
+            else if (this.radioButtonRed.Checked)
+                this.textBoxNumbers.ForeColor = Color.Red;
+            else if (this.radioButtonGreen.Checked)
+                this.textBoxNumbers.ForeColor = Color.Green;
+            else if (this.radioButtonBlue.Checked)
+                this.textBoxNumbers.ForeColor = Color.Blue;
+        }
+
+        private void checkBox_CheckedChanged(object sender, EventArgs e)
+        {
+            this.textBoxNumbers.Font = new Font(this.textBoxNumbers.Font, FontStyle.Regular);
+            if (this.checkBoxBold.Checked)
+                this.textBoxNumbers.Font = new Font(this.textBoxNumbers.Font, this.textBoxNumbers.Font.Style | FontStyle.Bold);
+            if (this.checkBoxItalic.Checked)
+                this.textBoxNumbers.Font = new Font(this.textBoxNumbers.Font, this.textBoxNumbers.Font.Style | FontStyle.Italic);
+        }
+    }
+}
