aca56c9e9bc6ed8aef9a873e8a2c3db39fb18caf
Safak TURKELI <sfktrkl@gmail.com>
2022-02-06 01:19:54 +0300

    MSSQL Insert

---
+++ MSSQL/Insert.sql
@@ -0,0 +1,9 @@
+insert into Job(Name) Values('Author')
+insert into Job(Name) Values('Scientist')
+insert into Job(Name) Values('Comedian')
+
+insert into First(Name, Surname, Job) Values('Lev', 'Tolstoy', 1)
+insert into First(Name, Surname, Job) Values('Charles', 'Dickens', 1)
+insert into First(Name, Surname, Job) Values('Albert', 'Einstein', 2)
+insert into First(Name, Surname, Job) Values('Jim', 'Carry', 3)
+insert into First(Name, Job) Values('Limmy', 3)
